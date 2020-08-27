27d24ef417a440cd2eb3f48fd58c36affb990aed
reason <reasonz@daum.net>
2020-08-28 01:42:49 +0900

    Add dashboard table (fixed #23)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React,{Component} from 'react';
-import Board from './components/board';
-import './App.css';
-import Main from './components/main';
-import Detail from './components/detail/detail';
-import Listview from './components/list-view/list-view';
-import Dashboard from './components/dashboard';
+import React, { Component } from "react";
+import Board from "./components/board";
+import "./App.css";
+import Main from "./components/main";
+import Detail from "./components/detail/detail";
+import Listview from "./components/list-view/list-view";
+import Dashboard from "./components/dashboard";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <Main/>
+        <Main />
         {/* <Board/> */}
         {/* <Detail /> */}
         {/* <Listview /> */}
